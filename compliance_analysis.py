bde8711793426e57fd72d4c0b3bc3bb62d5b9d1c
wlmbrown <103210243+wlmbrown@users.noreply.github.com>
2024-08-14 13:48:06 -0400

    new requirements added to Project CC and Data CC

--- compliance_analysis.py
+++ compliance_analysis.py
@@ -1,5 +1,5 @@
 import yaml
-from utils import set_type, set_operator_role_and_location, set_eu_market_status, check_within_scope, check_prohibited
+from utils import set_type, set_operator_role_and_location, set_eu_market_status, check_within_scope
 
 # Create some variables we will use throughout our analysis
 
@@ -25,6 +25,8 @@ project_variables = {
     }
 }
 
+project_intended_purpose = None 
+
 def run_compliance_analysis_on_project(project_cc_yaml):
 
     # Determine project type (AI system vs. GPAI model) as well as operator type. We will use these for different things.
@@ -33,22 +35,25 @@ def run_compliance_analysis_on_project(project_cc_yaml):
     set_eu_market_status(project_variables, project_cc_yaml)
 
     # Check if the project is within scope of the Act. If it's not, the analysis is over.
-    if check_within_scope(project_variables, project_cc_yaml):
+    if check_within_scope(project_cc_yaml):
         msg = ("Project is within the scope of Act. Let's continue...") 
     else: 
         msg = ("Project is not within the scope of what is regulated by the Act.")
-        return msg
-    
-    # Check for prohibited practices. If any exist, the analysis is over.
-    if check_prohibited(project_variables, project_cc_yaml) == True:
-        msg = ("Project is non-compliant due to a prohibited practice.")
-        return msg
+
+    # # Check for prohibited practices. If any exist, the analysis is over.
+    # if check_prohibited(project_cc_yaml) == True: 
+    #     print("Project contains prohibited practices and is therefore non-compliant.")
+    #     msg = ("Project is non-compliant due to a prohibited practice.")
+    # else: 
+    #     print("Project does not contain prohibited practies. Let's continue...")
 
     # If project is high-risk AI system, check that is has met all the requirements for such systems: 
 
     if project_type == "high_risk_ai_system":
 
     # Do this by examining the Project CC
+
+        # WE HAVE TO ADD A CONDITION THAT APPLIES THESE RULES BELOW ONLY IF operator_role == provider
 
         for key, value in project_cc_yaml['risk_management_system']:
             if not value:
@@ -110,6 +115,107 @@ def run_compliance_analysis_on_model(model_cc_yaml):
             msg = (f"Because of the model represented by , this high-risk  AI system fails the quality management requirements under Article 17.")
    
     return msg
+
+    def_check_intended_use_aligned():  # We want to run this function for everything classified as a high_risk_ai_system
+                                        # We also need to run it for all 
+
+        # Add any of the intended purposes of the overall project to a set of intended purposes 
+
+        if project_cc_yaml['high_risk_ai_system']['safety_component'] == True:
+            intended_purpose = "safety_component"
+        if project_cc_yaml['high_risk_ai_system']['product_regulated_machinery'] == True:
+            intended_purpose = "product_regulated_machinery" 
+        if project_cc_yaml['high_risk_ai_system']['product_regulated_toy'] == True:
+            intended_purpose = "product_regulated_toy" 
+        if project_cc_yaml['high_risk_ai_system']['product_regulated_watercraft'] == True:
+            intended_purpose = "product_regulated_watercraft" 
+        if project_cc_yaml['high_risk_ai_system']['biometric_categorization'] == True:
+            intended_purpose = "biometric_categorization" 
+        if project_cc_yaml['high_risk_ai_system']['emotion_recognition'] == True:
+            intended_purpose = "emotion_recognition" 
+        if project_cc_yaml['high_risk_ai_system']['critical_infrastructure'] == True:
+            intended_purpose = "critical_infrastructure" 
+        if project_cc_yaml['high_risk_ai_system']['admission'] == True:
+            intended_purpose = "admission" 
+        if project_cc_yaml['high_risk_ai_system']['recruitment'] == True:
+            intended_purpose = "recruitment" 
+        if project_cc_yaml['high_risk_ai_system']['public_assistance'] == True:
+            intended_purpose = "public_assistance"
+        if project_cc_yaml['high_risk_ai_system']['victim_assessment'] == True:
+            intended_purpose = "victim_assessment" 
+        if project_cc_yaml['high_risk_ai_system']['polygraph'] == True:
+            intended_purpose = "polygraph" 
+        if project_cc_yaml['high_risk_ai_system']['judicial'] == True:
+            intended_purpose = "judicial" 
+        
+        # For each Data CC, put the intended uses in a set and then make sure the Project's intended use is in the set
+
+        dataset_intended_purpose = () 
+
+        if data_cc_yaml['intended_purpose']['safety_component'] == True:
+            dataset_intended_purpose.add("safety_component")
+        if data_cc_yaml['intended_purpose']['product_regulated_machinery'] == True:
+            dataset_intended_purpose.add("product_regulated_machinery")
+        if data_cc_yaml['intended_purpose']['product_regulated_toy'] == True:
+            dataset_intended_purpose.add("product_regulated_toy")
+        if data_cc_yaml['intended_purpose']['product_regulated_watercraft'] == True:
+            dataset_intended_purpose.add("product_regulated_watercraft")
+        if data_cc_yaml['intended_purpose']['biometric_categorization'] == True:
+            dataset_intended_purpose.add("biometric_categorization")
+        if data_cc_yaml['intended_purpose']['emotion_recognition'] == True:
+            dataset_intended_purpose.add("emotion_recognition")
+        if data_cc_yaml['intended_purpose']['critical_infrastructure'] == True:
+            dataset_intended_purpose.add("critical_infrastructure")
+        if data_cc_yaml['intended_purpose']['admission'] == True:
+            dataset_intended_purpose.add("admission")
+        if data_cc_yaml['intended_purpose']['recruitment'] == True:
+            dataset_intended_purpose.add("recruitment")
+        if data_cc_yaml['intended_purpose']['public_assistance'] == True:
+            dataset_intended_purpose.add("public_assistance")       
+        if data_cc_yaml['intended_purpose']['victim_assessment'] == True:
+            dataset_intended_purpose.add("victim_assessment")
+        if data_cc_yaml['intended_purpose']['polygraph'] == True:
+            dataset_intended_purpose.add("polygraph")
+        if data_cc_yaml['intended_purpose']['judicial'] == True:
+            dataset_intended_purpose.add("judicial")
+
+        if project_intended_purpose not in dataset_intended_purpose:
+            print("You are not compliant")
+
+        # Now do the exact same thing for all models
+
+        model_intended_purpose = () 
+
+        if model_cc_yaml['intended_purpose']['safety_component'] == True:
+            dataset_intended_purpose.add("safety_component")
+        if model_cc_yaml['intended_purpose']['product_regulated_machinery'] == True:
+            dataset_intended_purpose.add("product_regulated_machinery")
+        if model_cc_yaml['intended_purpose']['product_regulated_toy'] == True:
+            dataset_intended_purpose.add("product_regulated_toy")
+        if model_cc_yaml['intended_purpose']['product_regulated_watercraft'] == True:
+            dataset_intended_purpose.add("product_regulated_watercraft")
+        if model_cc_yaml['intended_purpose']['biometric_categorization'] == True:
+            dataset_intended_purpose.add("biometric_categorization")
+        if model_cc_yaml['intended_purpose']['emotion_recognition'] == True:
+            dataset_intended_purpose.add("emotion_recognition")
+        if model_cc_yaml['intended_purpose']['critical_infrastructure'] == True:
+            dataset_intended_purpose.add("critical_infrastructure")
+        if model_cc_yaml['intended_purpose']['admission'] == True:
+            dataset_intended_purpose.add("admission")
+        if model_cc_yaml['intended_purpose']['recruitment'] == True:
+            dataset_intended_purpose.add("recruitment")
+        if model_cc_yaml['intended_purpose']['public_assistance'] == True:
+            dataset_intended_purpose.add("public_assistance")       
+        if model_cc_yaml['intended_purpose']['victim_assessment'] == True:
+            dataset_intended_purpose.add("victim_assessment")
+        if model_cc_yaml['intended_purpose']['polygraph'] == True:
+            dataset_intended_purpose.add("polygraph")
+        if model_cc_yaml['intended_purpose']['judicial'] == True:
+            dataset_intended_purpose.add("judicial")
+
+        if project_intended_purpose not in dataset_intended_purpose:
+            print("You are not compliant")
+
 
 
     # # If the project is a GPAI model, check that is has met all the requirements for such systems: 
