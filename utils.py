90fa1b0f089c731c2fc6d41f82aa19a6b7d43ff9
qqubb <123086401+qqubb@users.noreply.github.com>
2024-09-10 12:16:25 +0100

    update check_excepted

--- utils.py
+++ utils.py
@@ -54,7 +54,9 @@ def check_within_scope_act(dispositive_variables, project_cc_yaml):
 
 def check_excepted(dispositive_variables, project_cc_yaml):
     
-    if (project_cc_yaml['excepted']['scientific']['value'] or 
+    if( (project_cc_yaml['ai_system']['ai_system']['value'] == True and project_cc_yaml['excepted']['military']['value']) or
+        (project_cc_yaml['ai_system']['ai_system']['value'] == True and project_cc_yaml['excepted']['military_use']['value']) or
+        project_cc_yaml['excepted']['scientific']['value'] or 
         project_cc_yaml['excepted']['pre_market']['value'] or 
         (project_cc_yaml['ai_system']['ai_system']['value'] == True and 
          project_cc_yaml['excepted']['open_source_ai_model']['value']  == True) or 
